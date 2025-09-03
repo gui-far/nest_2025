1d6fe5dcab504d53f0073061051cefc9509669c3
gui-far <gui.far@outlook.com>
2025-09-03 20:04:08 -0300

    Zod validations with PIPES

---
+++ src/pipes/zod-validation-pipe.ts
@@ -0,0 +1,36 @@
+import {
+    PipeTransform,
+    ArgumentMetadata,
+    BadRequestException,
+} from '@nestjs/common'
+
+import { ZodError, ZodObject, ZodSchema } from 'zod'
+import { fromZodError } from 'zod-validation-error'
+
+export class ZodValidationPipe implements PipeTransform {
+
+    constructor(private schema: ZodSchema) { }
+
+    transform(value: any) {
+        try {
+
+            this.schema.parse(value)
+
+        } catch (error) {
+
+            if (error instanceof ZodError) {
+                throw new BadRequestException({
+                    message: 'Validation failed',
+                    statusCode: 400,
+                    errors: fromZodError(error),
+                })
+            }
+
+            throw new BadRequestException('Validation failed')
+
+        }
+
+        return value
+    }
+
+}
