6ed870e0a4a07cbc3acb2a4f3db01bc0c373f29b
gui-far <gui.far@outlook.com>
2025-09-03 21:47:35 -0300

    Creating public and private keys, adding the keys to the AuthModule, defining the jwt ans RS256, and creating the Authentication Controller creating a generic jwt for testing

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,34 @@
 import { Module } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { Env } from 'src/env';
 
-@Module({})
-export class AuthModule {}
+@Module({
+    imports: [
+        PassportModule,
+        JwtModule.registerAsync({
+            inject: [ConfigService],
+            global: true,
+            useFactory(config: ConfigService<Env, true>) {
+
+                const secret = config.get('JWT_SECRET', { infer: true })
+
+                const privateKey = config.get('JWT_PRIVATE_KEY', { infer: true })
+
+                const publicKey = config.get('JWT_PUBLIC_KEY', { infer: true })
+
+                return {
+                    signOptions: { algorithm: 'RS256' },
+                    privateKey: Buffer.from(privateKey, 'base64'),
+                    publicKey: Buffer.from(publicKey, 'base64')
+                }
+            }
+        })
+    ]
+})
+export class AuthModule {
+
+
+
+}
